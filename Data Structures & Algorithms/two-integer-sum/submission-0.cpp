4351d7b9dc3648048f1972fc4525bec41dd9e71d
AthSat <110881105+Atharv-Sathe@users.noreply.github.com>
2026-05-26 23:03:04 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> visited;
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited.count(target - nums[i])) {
+                return {visited[target - nums[i]], i};
+            } 
+            visited[nums[i]] = i;
+        }
+        return {};
+    }
+};
