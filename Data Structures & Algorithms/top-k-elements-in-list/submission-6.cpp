c3cd9620853c66eddad74030a6a1120b0f04f593
AthSat <110881105+Atharv-Sathe@users.noreply.github.com>
2026-07-01 00:34:15 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // unordered_map<int, int> freq;
+        // for (int i : nums) {
+        //     freq[i]++;
+        // }
+        // vector<pair<int, int>> freq_to_num;
+        // for (auto p : freq) {
+        //     freq_to_num.push_back(pair<int, int>(p.second, p.first));
+        // }
+        // sort(freq_to_num.begin(), freq_to_num.end(), greater<>());
+        // vector<int> ans;
+        // for (int i = 0; i < k; i++) {
+        //     ans.push_back(freq_to_num[i].second);
+        // }
+        // return ans;
+
+        // vector<int> freq(2001, 0);
+        // for (int i = 0; i < nums.size(); i++) {
+        //     freq[nums[i] + 1000]++;
+        // }
+
+        // priority_queue<pair<int, int>> pq;
+        // for (int i = 0; i < 2001; i++) {
+        //     if (!freq[i]) continue;
+        //     pq.push(pair<int, int>(freq[i], i - 1000));
+        // }
+
+        // vector<int> ans;
+        // while(k--) {
+        //     auto [freq, num] = pq.top();
+        //     pq.pop();
+        //     ans.push_back(num);
+        // }
+        // return ans;
+
+        unordered_map<int,int> freq;
+        for (int n : nums) freq[n]++;
+
+        vector<vector<int>> buckets(nums.size() + 1);
+        for (auto& [num, f] : freq) buckets[f].push_back(num);
+
+        vector<int> ans;
+        for (int f = buckets.size() - 1; f >= 0 && ans.size() < k; f--)
+            for (int num : buckets[f]) {
+                ans.push_back(num);
+                if (ans.size() == k) break;
+            }
+        return ans;
+    }
+};
